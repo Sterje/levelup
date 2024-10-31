574422bf87fd544c1a7f09f29935b7c2f2d5297e
Jesper Eriksson Stenberg <jespermail@gmail.com>
2024-10-31 21:23:31 +0100

    Small changes

--- script3.js
+++ script3.js
@@ -9,5 +9,5 @@ function calculate() {
     let oneDecimal = number.toFixed(1); // Avrundar till en decimal, funkar ej som det ska
     
     // Visa resultaten
-    document.getElementById("results").innerHTML = "Här ska talet avrundas till heltal men avrundar bara nedåt, alltså fel " + heltal + "<br>" + "Här ska talet avrundas till en decimal, fungerar ej korrekt. " + oneDecimal;
+    document.getElementById("results").innerHTML = "Talet avrundat till heltal " + heltal + "<br>" + "Talet avrundat till en decimal. " + oneDecimal;
 }
